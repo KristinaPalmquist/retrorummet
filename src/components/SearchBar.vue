706ef5d2ed99f1230eec8753bc0fcf9ffef78746
Kristina Palmquist <kristina.palmquist@gmail.com>
2025-01-13 18:35:42 +0100

    Added media query for very small screens

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -58,8 +58,14 @@ watch(
 @media only screen and (max-width: 600px) {
   #search-bar .input-field {
     background: var(--color-background);
-    width: 200px;
+    /* width: 200px; */
     box-shadow: 0 12px 32px rgba(0, 0, 0, 0.1);
+  }
+}
+
+@media only screen and (max-width: 400px) {
+  #search-bar .input-field {
+    width: 175px;
   }
 }
 </style>
